906685456718a461a9ce93a30491e11364004877
Nick Sagona <nick@nolainteractive.com>
2016-06-06 17:07:27 -0500

    Update HTTP response codes

--- src/Response.php
+++ src/Response.php
@@ -34,6 +34,7 @@ class Response
         // Informational 1xx
         100 => 'Continue',
         101 => 'Switching Protocols',
+        102 => 'Processing',
 
         // Success 2xx
         200 => 'OK',
@@ -43,6 +44,9 @@ class Response
         204 => 'No Content',
         205 => 'Reset Content',
         206 => 'Partial Content',
+        207 => 'Multi-Status',
+        208 => 'Already Reported',
+        226 => 'IM Used',
 
         // Redirection 3xx
         300 => 'Multiple Choices',
@@ -51,7 +55,9 @@ class Response
         303 => 'See Other',
         304 => 'Not Modified',
         305 => 'Use Proxy',
+        306 => 'Switch Proxy',
         307 => 'Temporary Redirect',
+        308 => 'Permanent Redirect',
 
         // Client Error 4xx
         400 => 'Bad Request',
@@ -67,11 +73,20 @@ class Response
         410 => 'Gone',
         411 => 'Length Required',
         412 => 'Precondition Failed',
-        413 => 'Request Entity Too Large',
-        414 => 'Request-URI Too Long',
+        413 => 'Payload Too Large',
+        414 => 'URI Too Long',
         415 => 'Unsupported Media Type',
-        416 => 'Requested Range Not Satisfiable',
+        416 => 'Range Not Satisfiable',
         417 => 'Expectation Failed',
+        421 => 'Misdirected Request',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        451 => 'Unavailable For Legal Reasons',
 
         // Server Error 5xx
         500 => 'Internal Server Error',
@@ -80,7 +95,12 @@ class Response
         503 => 'Service Unavailable',
         504 => 'Gateway Timeout',
         505 => 'HTTP Version Not Supported',
-        509 => 'Bandwidth Limit Exceeded'
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        509 => 'Bandwidth Limit Exceeded',
+        510 => 'Not Extended',
+        511 => 'Network Authentication Required'
     ];
 
     /**
