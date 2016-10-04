78d39894373b2df5b99ef47ce5a91a1c5eae16b9
Nick Sagona <nick@nolainteractive.com>
2016-10-04 11:06:51 -0500

    Update docblocks

--- src/Upload.php
+++ src/Upload.php
@@ -17,11 +17,11 @@ namespace Pop\Http;
  * HTTP upload class
  *
  * @category   Pop
- * @package    Pop_Http
+ * @package    Pop\Http
  * @author     Nick Sagona, III <dev@nolainteractive.com>
  * @copyright  Copyright (c) 2009-2016 NOLA Interactive, LLC. (http://www.nolainteractive.com)
  * @license    http://www.popphp.org/license     New BSD License
- * @version    2.1.0
+ * @version    3.0.0
  */
 class Upload
 {
